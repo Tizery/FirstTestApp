564c3af09873ad2befa2562cc516e5db87456d7a
Tizery <reserved123@mail.ru>
2022-04-20 18:13:51 +0700

    hw1

---
+++ app/src/main/java/com/example/firsttestapp/VerificationClass.kt
@@ -0,0 +1,14 @@
+package com.example.firsttestapp
+
+class VerificationClass {
+
+    companion object {
+
+        var obj1 = "Junit"
+        var obj2 = "Junit"
+        var obj3 = "Hello"
+        var obj4 = arrayOf(1, 2, 3, 4, 5)
+        var obj5 = arrayOf(1, 2, 3, 4, 5)
+        var obj6: String? = null
+    }
+}
